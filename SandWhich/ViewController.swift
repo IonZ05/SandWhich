d9e635e4a50efacf32e97a6598aeb99e2fbc8dd2
Ian <ivargas16@cps.edu>
2022-07-11 12:10:36 -0500

    Initial Commit

---
+++ SandWhich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SandWhich
+//
+//  Created by DPI Student 22 on 7/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
